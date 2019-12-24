6208c0e1ecb27bd6ddb445d6bd864516c55270e3
Peter Kim <36287463+Peter-Kim321@users.noreply.github.com>
2019-12-24 23:21:48 +1100

    update models.py

--- AdminSystem/AdminSystemApp/models.py
+++ AdminSystem/AdminSystemApp/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class CadetsList(models.Model):
     firstName = models.CharField(max_length=50)
     lastName = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.firstName
