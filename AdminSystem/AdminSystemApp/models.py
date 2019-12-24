9c58825b1f07224c4af9bc568808db530d957a99
Peter Kim <36287463+Peter-Kim321@users.noreply.github.com>
2019-12-24 23:02:30 +1100

    sqlite init

--- AdminSystem/AdminSystemApp/models.py
+++ AdminSystem/AdminSystemApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class CadetsList(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
