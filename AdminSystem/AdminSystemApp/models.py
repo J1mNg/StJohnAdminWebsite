c7441bb8b38ee6d823db2420392cd4547545543e
Ubuntu <ubuntu@ip-172-31-85-12.ec2.internal>
2019-12-25 07:03:04 +0000

    sqlite cadet database established

--- AdminSystem/AdminSystemApp/models.py
+++ AdminSystem/AdminSystemApp/models.py
@@ -2,8 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Cadet(models.Model):
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
+    user_id = models.AutoField(primary_key=True)
+    firstname = models.CharField(max_length=50, default="null")
+    lastname = models.CharField(max_length=50, default="null")
+    birthday = models.DateTimeField(auto_now=True)
+    age = models.IntegerField(default=0)
+    mobile = models.IntegerField(default=0)
+    email = models.EmailField(max_length=254, default='default@example.com')
+    date_joined = models.DateTimeField(auto_now=True)
+    years_since_joined = models.IntegerField(default=0)
+    bond_paid = models.DateTimeField(auto_now=True)
+    joining_fee_paid = models.DateTimeField(auto_now=True)
+    rank = models.CharField(max_length=50)
+    qualification = models.CharField(max_length=50, default="null")
+    total_duty_hours = models.IntegerField(default=0)
+    num_of_events = models.IntegerField(default=0)
+    training_hours = models.IntegerField(default=0)
+    other_hours = models.IntegerField(default=0)
+    meeting_hours = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.firstName + " " + self.lastName
+        return self.user_id + ": " + self.firstname + " " + self.lastname
--- AdminSystem/AdminSystemApp/views.py
+++ AdminSystem/AdminSystemApp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+from AdminSystemApp.models import Cadet
 
 # Create your views here.
 
@@ -7,11 +10,14 @@ def home(request):
     return render(request, "home.html")
 
 def cadets(request):
-    return render(request, "cadets.html")
+    cadets = Cadet.objects.all()
+
+    args = { 'name': name }
+    return render(request, "cadets.html", args)
+
 
 def role_marking(request):
     return render(request, "role_marking.html")
 
 def cash_box(request):
     return render(request, "cash_box.html")
-
