dff108950a708e4aabc2923acbc8d690d4f48018
Ubuntu <ubuntu@ip-172-31-85-12.ec2.internal>
2020-01-04 11:12:54 +0000

    rewards app basics done:

--- AdminSystem/static/js/table_toggle.js
+++ AdminSystem/static/js/table_toggle.js
@@ -4,11 +4,19 @@ var tableB = document.getElementById("tableB");
 var btnA = document.getElementById("toggleA");
 var btnB = document.getElementById("toggleB");
 
+var headingA = document.getElementById("headingA")
+
 btnA.onclick = function () {
     tableA.style.display = "table";
     tableB.style.display = "none";
+    btnA.className='btn btn-primary';
+    btnB.className='btn btn-secondary';
+    headingA.innerHTML='Cadets who are close to next reward tier'
 }
 btnB.onclick = function () {
     tableA.style.display = "none";
     tableB.style.display = "table";
+    btnB.className='btn btn-primary';
+    btnA.className='btn btn-secondary';
+    headingA.innerHTML='Cadets who have yet to recieve their rewards'
 }
